4542cf8c00f8e67db78c4d89d7ee8de593249a98
Carlos Aurélio <carlosync@gmail.com>
2021-10-06 21:45:40 -0400

    Create project with Spring Boot

---
+++ src/main/java/com/sw2you/wsspringboot/WsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sw2you.wsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw2you/wsspringboot/WsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw2you.wsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
