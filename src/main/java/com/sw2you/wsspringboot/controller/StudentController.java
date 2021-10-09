c62ed7558c3586d57cd8ee56495211195f37900d
Carlos Aurélio <carlosync@gmail.com>
2021-10-09 18:29:19 -0400

    Create file configuration database, start Student table

--- src/main/java/com/sw2you/wsspringboot/controller/StudentController.java
+++ src/main/java/com/sw2you/wsspringboot/controller/StudentController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -17,6 +15,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/sw2you/wsspringboot/model/Student.java
+++ src/main/java/com/sw2you/wsspringboot/model/Student.java
@@ -1,9 +1,15 @@
 package com.sw2you.wsspringboot.model;
-
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
+@Entity
+@Table
+public class Student implements Serializable {
 
-public class Student {
-
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
     private Long id;
     private String name;
     private String email;
